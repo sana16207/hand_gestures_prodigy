bcc91662b9c8fbb223b6200639fdab9cb83bd761
sana16207 <sana216007@gmail.com>
2026-05-20 14:10:02 +0530

    prepare render deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,6 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-export default defineConfig({});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
